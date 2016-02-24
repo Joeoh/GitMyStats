bbd487cb85e0af9ad377d06ad6e715da152ce20f
Jeremy <barischj@tcd.ie>
2016-02-24 15:29:52 +0000

    rewrote comment

--- test/mocha-async-helper.js
+++ test/mocha-async-helper.js
@@ -1,20 +1,19 @@
 /*
-  We have to do something weird in asynchronous tests because of a known bug
-  when using `mocha` the test runner with `chai` as the assertion library.
-  Ideally you'd like to write something like the following in a callback:
-    
-      assert.isOk("some assertion", "failure message");
-      done();
 
-  Since the test is asynchronous we need to let `mocha` the test runner
-  know when the test is finished. This is done by calling the `done`
-  callback, as you can see above. This is the normal way of writing an
-  asynchonous callback.
+  When writing asynchronous tests  we need to let `mocha` the test runner know
+  when the test is finished. This is done by calling the `done` function which
+  `mocha` provides.
 
-  What the bug does is that if an `assert` call fails an exception is raised
-  that is not caught by `mocha` the test runner. So we have to catch the
-  exception ourselves and return it to `mocha` through the `done` callback. The
-  following illustrates this approach:
+      function() {
+          assert.isOk("some assertion", "failure message");
+          done();
+      }
+  
+  However we have to do something weird because of a known bug when using
+  `mocha` with `chai` as the assertion library. What the bug does is that if an
+  `assert` call fails an exception is raised that is not caught by `mocha`. So
+  we have to catch the exception ourselves and return it to `mocha` through the
+  `done` callback.
     
       try {
         assert.isOk("some assertion", "failure message");
