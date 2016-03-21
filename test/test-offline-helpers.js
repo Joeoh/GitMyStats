864b832572a33839ed94c89bdb31c31df870ea4d
Jeremy Barisch-Rooney <barischj@tcd.ie>
2016-03-21 15:28:33 +0000

    Update test-offline-helpers.js

--- test/test-offline-helpers.js
+++ test/test-offline-helpers.js
@@ -1,11 +1,11 @@
 const assert        = require("chai").assert
 const helpers       = require("../src/helpers.js")
 
-describe("trimArray", function() {
-    it("should trim an array", function() {
-        var a = [0, 1, 0, 1, 9, 9, 7, 6, 7, 6]
-        assert.deepEqual([9, 9], a.trimEnds(function(x) {
+describe("Array.prototype.trimEnds", function() {
+    it("should return a new array with ends removed based on a given condition", function() {
+        var a = [0, 1, 0, 1, 10, 1, 9, 7, 6, 7, 6]
+        assert.deepEqual([10, 1, 9], a.trimEnds(function(x) {
             return x < 9
-        }), "should only leave 9s")
+        }), "remove ends less than 9")
     });
 });
